c8903bde7c36dd75b7382299fbd0c9cbc02ba23d
Leonardo Lemos <56661618+LeonardoLemosDev@users.noreply.github.com>
2025-04-18 17:33:05 -0300

    first commit

---
+++ src/main/java/com/leonardodeoliveira97/task_queue_system/TaskQueueSystemApplication.java
@@ -0,0 +1,13 @@
+package com.leonardodeoliveira97.task_queue_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskQueueSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskQueueSystemApplication.class, args);
+	}
+
+}
