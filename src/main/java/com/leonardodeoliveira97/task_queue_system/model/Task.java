ebf9dda5d83f9bad4cb6ad062322dd6e522af700
Leonardo Lemos <56661618+LeonardoLemosDev@users.noreply.github.com>
2025-04-18 22:30:26 -0300

    Creation of the Task model and the TaskStatus enum

---
+++ src/main/java/com/leonardodeoliveira97/task_queue_system/model/Task.java
@@ -0,0 +1,23 @@
+package com.leonardodeoliveira97.task_queue_system.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime processedAt;
+}
---
+++ src/main/java/com/leonardodeoliveira97/task_queue_system/model/TaskStatus.java
@@ -0,0 +1,8 @@
+package com.leonardodeoliveira97.task_queue_system.model;
+
+public enum TaskStatus {
+    PENDING,
+    PROCESSING,
+    COMPLETED,
+    FAILED
+}
